77a41d20ff4c990a527ad2422b9f1cd3c42f613a
Claudenir Freitas <claudenirfreitas.cf@gmail.com>
2019-05-21 23:09:08 -0300

    [APX-19] - alterando config para priorizar variaveis de ambiente

--- config/config.js
+++ config/config.js
@@ -1,10 +1,9 @@
 const dotenv = require('dotenv');
-const result = dotenv.config();
+const { parsed: envs } = dotenv.config();
 
-if (result.error) {
-    throw result.error;
-}
-
-const { parsed: envs } = result;
-
-module.exports = envs;
+module.exports = {
+    RABBIT_SERVER: process.env.RABBIT_SERVER || envs.RABBIT_SERVER,
+    RABBIT_KIT_QUEUE: process.env.RABBIT_KIT_QUEUE || envs.RABBIT_KIT_QUEUE,
+    RABBIT_SPECIFICATION_QUEUE: process.env.RABBIT_SPECIFICATION_QUEUE || envs.RABBIT_SPECIFICATION_QUEUE,
+    RABBIT_RECOMMENDATION_QUEUE: process.env.RABBIT_RECOMMENDATION_QUEUE || envs.RABBIT_RECOMMENDATION_QUEUE
+};
