3463c5bff497b22e9108d6fdc53fe3a566e9dcb5
Claudenir Freitas <claudenirfreitas.cf@gmail.com>
2019-05-21 23:36:19 -0300

    [APX-19] - melhoria para aplicacao rodar em container

--- service/kit-search.js
+++ service/kit-search.js
@@ -2,7 +2,10 @@ const puppeteer = require('puppeteer');
 
 async function search(url, callback, gender, specification) {
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
 
     page.on('request', request => request.continue());
@@ -11,9 +14,9 @@ async function search(url, callback, gender, specification) {
     await page.goto(url.getCompleteUrl(), { waitUntil: 'load' });
 
     const html = await page.content();
-    
+
     browser.close();
-    
+
     return callback(html, url, gender, Date.now(), specification);
 }
 
