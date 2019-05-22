1dcf982133192639a45e74825e0e31393122f1e7
Claudenir Freitas <claudenirfreitas.cf@gmail.com>
2019-05-22 09:00:01 -0300

    [APX-19] - adicionando novo client http

--- service/kit-search.js
+++ service/kit-search.js
@@ -1,23 +1,8 @@
-const puppeteer = require('puppeteer');
+const phin = require('phin')
 
 async function search(url, callback, gender, specification) {
-
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
-    const page = await browser.newPage();
-
-    page.on('request', request => request.continue());
-
-    await page.setRequestInterception(true);
-    await page.goto(url.getCompleteUrl(), { waitUntil: 'load' });
-
-    const html = await page.content();
-
-    browser.close();
-
-    return callback(html, url, gender, Date.now(), specification);
+    const response = await phin(url.getCompleteUrl());
+    return callback(response.body, url, gender, Date.now(), specification);
 }
 
 module.exports = search;
